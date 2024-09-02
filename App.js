8c5acdeb44d401bb724f2c38e5509abac1bc9062
Amartya Upmanyu <upmanyu07amartya@gmail.com>
2024-09-02 20:37:32 +0530

    SpiceSavor initial structure

--- App.js
+++ App.js
@@ -1,44 +1,74 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { restaurants } from "./data";
 
-//React Element - React.createElement => object.
-// When we render this obj to DOM, then it becomes a HTML element.
-// JSX - Javascript syntax which is easier to create React elements.
-// JSX is not a part of react.
-// JSX is not HTML inside Javascript. It is like HTML like syntax.
-//browsers cant understand JSX as it is not JS code.
-// JSX is transpiled before going to JS engine using the package Babel
-
-//const heading = React.createElement("h1",{id:"heading"},"Namaste React");
-// const jsxHeading = (
-//   <h1 id="heading" className="head">
-//     Namaste React using JSX
-//   </h1>
-// );
-
-const TitleComponent = () => {
-  return <h2>Title</h2>;
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://i.pinimg.com/originals/34/0c/6a/340c6add7519212185a08d4205eb1965.png"
+        ></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
-// console.log("heading",heading)
-// console.log("jsx",jsxHeadding)
-
-// These 2 objs are one and the same thing
-
-//REACT COMPONENTS
-// CLASS BASED AND FUNCTION BASED
-
-// React Functional Component - It is a normal JS function which returns some JSX
-
-const HeadingComponent = () => {
+const RestaurantCard = ({ resData }) => {
   return (
-    <div>
-      <h1>Namaste React functional component</h1>
-      <TitleComponent />
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      {/* Inline style is a JS object */}
+      <img
+        className="res-logo"
+        alt="image"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/" +
+          resData.cloudinaryImageId
+        }
+      />
+
+      <h3>{resData.name}</h3>
+      <h4>{resData.cuisines.join(", ")}</h4>
+      <h4>{resData.avgRating} stars</h4>
+      <h4>{resData.costForTwo}</h4>
+      <h4>{resData.sla.deliveryTime} mins</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="Search">Search</div>
+      <div className="res-container">
+        {restaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant.info} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
     </div>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //root.render(jsxHeading) - this is how to render a element
-root.render(<HeadingComponent />); // this is how you render a component
+root.render(<AppLayout />); // this is how you render a component
