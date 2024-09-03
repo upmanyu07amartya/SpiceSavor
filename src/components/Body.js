33dd857edd663a081a6b9e3fcb1b2da2e312db71
Amartya Upmanyu <upmanyu07amartya@gmail.com>
2024-09-03 16:43:45 +0530

    fetching data from swiggy's API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,26 @@
-import { useState } from "react";
-import { restaurantsList } from "../utils/data";
+import { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
 
 export const Body = () => {
-  const [listOfRes, setListOfRes] = useState(restaurantsList);
+  const [listOfRes, setListOfRes] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+  //useEffect takes 2 arguments - the first argument is an arrow function.
+  //the second argument is a dependency array
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65420&lng=77.23730&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING#"
+    );
+    const json = await data.json();
+    const formattedData =
+      json.data.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+    setListOfRes(formattedData);
+  };
+
+  if(listOfRes.length === 0 ){
+    return <h1>Loading...</h1>
+  }
 
   return (
     <div className="body">
